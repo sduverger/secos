5fb954adcb19aa582183236d087491bbe7df60e2
Nicolas <nicolas.dureisseix@etu.enseeiht.fr>
2018-09-19 19:58:39 +0200

    question 2

--- tp0/tp.c
+++ tp0/tp.c
@@ -8,6 +8,18 @@ extern uint32_t __kernel_end__;
 
 void tp()
 {
-   debug("kernel mem [0x%x - 0x%x]\n", &__kernel_start__, &__kernel_end__);
-   debug("MBI flags 0x%x\n", info->mbi->flags);
+  multiboot_memory_map_t *start;
+	multiboot_memory_map_t *end;
+	
+	debug("kernel mem [0x%x - 0x%x]\n", &__kernel_start__, &__kernel_end__);
+	debug("MBI flags 0x%x\n", info -> mbi -> flags);
+   
+	start = (multiboot_memory_map_t *) info -> mbi -> mmap_addr;
+	end = (multiboot_memory_map_t *) ( info -> mbi -> mmap_addr + info -> mbi -> mmap_length);
+    
+  while (start < end) { 
+		debug("0x%llx - 0x%llx %d\n", start -> addr, start -> len, start -> type);
+		start++;
+	}
+
 }
